c7179af616c1a8c237fddc40ebbcf1983d9aedee
yunhwan <sky10240107@naver.com>
2023-07-31 16:45:39 +0900

    feat: 유효성 검사 기능 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv";
 import * as path from "path";
+import { ValidationPipe } from '@nestjs/common';
 
 
 //dotenv 환경설정
@@ -15,6 +16,13 @@ dotenv.config({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  //validationPipe를 전역으로 사용
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }))
+
+
   await app.listen(3000);
 }
 bootstrap();
