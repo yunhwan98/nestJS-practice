6ccd2997140fecd8f0ee2e61a5d162aa081d6364
yunhwan <sky10240107@naver.com>
2023-07-31 17:08:39 +0900

    feat: typeorm으로 db연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import emailConfig from './config/emailConfig';
 
 import { UsersModule } from './users/users.module';
 import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
@@ -18,6 +19,19 @@ import { validationSchema } from './config/validationSchema';
       //유효성 검사
       validationSchema,
     }),
+    //TypeOrmModule 가져오기
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,//'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USERNAME, //'root'
+      password: process.env.DATABASE_PASSWORD,//'test',
+      database: 'test',
+      entities: [__dirname + `/**/*.entity{.ts,.js}`],
+
+      //DB 스키마 동기화 여부(프로덕션에서는 false!)
+      synchronize: process.env.DATABASE_SYNCRONIZE === 'true',
+    })
   ],
   controllers: [],
   providers: [],
