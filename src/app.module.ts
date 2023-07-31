a14b5d8b4a986b99ed9e594a8551764286a07a26
yunhwan <sky10240107@naver.com>
2023-07-31 12:51:48 +0900

    feat: email모듈 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { EmailService } from './email/email.service';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EmailModule],
   controllers: [],
   providers: [],
 
